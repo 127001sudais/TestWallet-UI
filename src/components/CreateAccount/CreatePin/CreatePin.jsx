eca38d65ad0406240aea0a97670d3144088b93b9
Samurai <127001sudais01@gmail.com>
2023-11-07 09:09:16 +0800

    minor changes for createPin

--- src/components/CreateAccount/CreatePin/CreatePin.jsx
+++ src/components/CreateAccount/CreatePin/CreatePin.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+
 import PinEntry from "../../PIN/PinEntry";
+
+// Import Header
+import Header from "../../Header/Header";
 
 function CreatePin({ nextStep, prevStep }) {
   const [pin, setPin] = useState("");
@@ -20,20 +24,39 @@ function CreatePin({ nextStep, prevStep }) {
   };
 
   return (
-    <div>
+    <>
+      <Header />
       <form onSubmit={handleSubmit}>
-        <PinEntry
-          onClick={handleButtonClick}
-          onBackSpaceClick={handleBackSpaceClick}
-        />
-        <button className="bg-gray-200 p-4 rounded-2xl" type="submit">
-          Next
-        </button>
-        <button type="button" onClick={prevStep}>
-          back
-        </button>
+        <div className="flex flex-col items-center">
+          <PinEntry
+            onClick={handleButtonClick}
+            onBackSpaceClick={handleBackSpaceClick}
+          />
+          {/* button begins here */}
+          <div className="flex mt-2 font-semibold justify-between w-full md:w-1/2 lg:w-1/3">
+            {/* previous button begins here */}
+            <button
+              className="bg-gray-200 px-2 rounded-xl  hover:bg-gray-400"
+              type="button"
+              onClick={prevStep}
+            >
+              Back
+            </button>
+            {/* previous button ends here */}
+
+            {/* next button begins here */}
+            <button
+              className="bg-gray-200 px-2 rounded-xl hover:bg-gray-400"
+              type="submit"
+            >
+              Next
+            </button>
+            {/* next button ends here */}
+          </div>
+          {/* button ends here */}
+        </div>
       </form>
-    </div>
+    </>
   );
 }
 
