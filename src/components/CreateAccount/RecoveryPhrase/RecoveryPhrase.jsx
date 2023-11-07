9490a6961dd55e749f3a81fcf5ccd0505e1d0e8f
Samurai <127001sudais01@gmail.com>
2023-11-07 11:27:43 +0800

    UI improved for RecoveryPhrase section

--- src/components/CreateAccount/RecoveryPhrase/RecoveryPhrase.jsx
+++ src/components/CreateAccount/RecoveryPhrase/RecoveryPhrase.jsx
@@ -1,8 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { generateMnemonic } from "bip39";
 
+// header
+import Header from "../../Header/Header";
+
+//
+import Notice from "../../Notice/Notice";
+
 function RecoveryPhrase({ nextStep, prevStep }) {
   const [mnemonic, setMnemonic] = useState("");
+
+  const text1 =
+    "If your device is lost or broken these words recover your account on a different device";
+  const text2 =
+    "If anyone sees your 12 words create a new account and move your funds immediately";
+  const text3 = "Record in secret, offline in order:";
 
   useEffect(() => {
     const newMnemonic = generateMnemonic();
@@ -13,16 +25,54 @@ function RecoveryPhrase({ nextStep, prevStep }) {
     e.preventDefault();
     nextStep({ mnemonic });
   };
+
+  const words = mnemonic.split(" ");
   return (
-    <div>
+    <>
+      <Header />
+      <Notice text={text1} />
+      <Notice text={text2} />
+      <Notice text={text3} />
+
       <form onSubmit={handleSubmit}>
-        <p>Your recovery phrase is: {mnemonic}</p>
-        <button type="submit">Next</button>
-        <button type="button" onClick={prevStep}>
-          Back
-        </button>
+        <div className="flex flex-col items-center">
+          <p>Your recovery phrase is:</p>
+
+          {/* */}
+          <div className="flex flex-col justify-center">
+            {words.map((word, i) => (
+              <div key={i} className="m-1 p-1 border rounded">
+                <span className="font-semibold">{i + 1}. </span>
+                {word}
+              </div>
+            ))}
+          </div>
+
+          {/* button begins here */}
+          <div className="flex mt-2 font-semibold justify-between w-full md:w-1/2 lg:w-1/3">
+            {/* previous button begins here */}
+            <button
+              className="bg-gray-200 px-2 rounded-xl  hover:bg-gray-400"
+              type="button"
+              onClick={prevStep}
+            >
+              Back
+            </button>
+            {/* previous button ends here */}
+
+            {/* next button begins here */}
+            <button
+              className="bg-gray-200 px-2 rounded-xl hover:bg-gray-400"
+              type="submit"
+            >
+              Next
+            </button>
+            {/* next button ends here */}
+          </div>
+          {/* button ends here */}
+        </div>
       </form>
-    </div>
+    </>
   );
 }
 
