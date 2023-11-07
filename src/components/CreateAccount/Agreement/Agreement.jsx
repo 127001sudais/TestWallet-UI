a912786b2bfe5e44dc123ca6034743d843d058bd
Samurai <127001sudais01@gmail.com>
2023-11-07 12:10:41 +0800

    improved UI for agreement

--- src/components/CreateAccount/Agreement/Agreement.jsx
+++ src/components/CreateAccount/Agreement/Agreement.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
 
+// icon
+import HandshakeRoundedIcon from "@mui/icons-material/HandshakeRounded";
+
+// Header
+import Header from "../../Header/Header";
+
+// Notice
+import Notice from "../../Notice/Notice";
+
+// css
+import "../../../styles/CreateAccount/Agreement.css";
+
 function Agreement({ nextStep, prevStep }) {
   const [agreed, setAgreed] = useState(false);
+  const [error, setError] = useState("");
 
   const handleAgreeChange = (e) => {
     setAgreed(e.target.checked);
@@ -12,31 +25,65 @@ function Agreement({ nextStep, prevStep }) {
     if (agreed) {
       nextStep();
     } else {
-      alert("You must agree to the terms and conditions to proceed");
+      setError("You must agree to the terms and conditions to proceed");
+      setTimeout(() => {
+        document.querySelector(".error-message").classList.add("show");
+      }, 0);
     }
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Terms and conditions</h1>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit,
-          optio?
-        </p>
-        <label>
-          <input
-            type="checkbox"
-            checked={agreed}
-            onChange={handleAgreeChange}
-          />
-          I agree to the terms and conditions
-        </label>
-        <button type="submit">Next</button>
-        <button type="button" onClick={prevStep}>
-          Back
-        </button>
+    <>
+      <Header />
+      <form className="mt-4" onSubmit={handleSubmit}>
+        <div className="bg-gray-200 rounded-md p-1">
+          <h1 className="font-bold text-center">Terms and conditions</h1>
+          <div className="flex">
+            <div className="m-auto">
+              <HandshakeRoundedIcon />
+            </div>{" "}
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit,
+              optio?
+            </p>
+          </div>
+          <label className="text-xs">
+            <input
+              type="checkbox"
+              checked={agreed}
+              onChange={handleAgreeChange}
+            />
+            I agree to the terms and conditions
+          </label>
+        </div>
+        {error && (
+          <div className="error-message">
+            <Notice text={error} />
+          </div>
+        )}
+        {/* button begins here */}
+        <div className="flex mt-12 font-semibold justify-between w-full md:w-1/2 lg:w-1/3">
+          {/* previous button begins here */}
+          <button
+            className="bg-gray-200 px-2 rounded-xl  hover:bg-gray-400"
+            type="button"
+            onClick={prevStep}
+          >
+            Back
+          </button>
+          {/* previous button ends here */}
+
+          {/* next button begins here */}
+          <button
+            className="bg-gray-200 px-2 rounded-xl hover:bg-gray-400"
+            type="submit"
+          >
+            Next
+          </button>
+          {/* next button ends here */}
+        </div>
+        {/* button ends here */}
       </form>
-    </div>
+    </>
   );
 }
 
